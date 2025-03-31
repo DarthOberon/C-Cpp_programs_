3c937f3c7b1688a4a393ca822499d4d67f51b232
DarthOberon <10aryansharma@gmail.com>
2025-03-31 10:28:17 +0530

    practical 8 completed.

--- even_odd_cp.cpp
+++ even_odd_cp.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter any Integer:";
+
+    cin >> n;
+
+    cout << "The number you entered is:" << n;
+
+    if(n%2 == 0)
+    {
+        cout << "The number is Even";
+    }
+    else{
+        cout << "The number is Odd";
+    }
+    return 0;
+    
+}
 
