f6f03a37d0227641ad610c0771a7202f900ca685
DarthOberon <10aryansharma@gmail.com>
2025-04-14 13:37:05 +0530

    changed the name of the file to easily identify

---
+++ practical_8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter any Integer:" << endl;
+
+    cin >> num;
+
+    if(num % 2 == 0)
+    {
+        cout << "The number "<< num <<" is Even"<< endl;
+    }
+    else{
+        cout << "The number "<< num <<" is Odd"<< endl;
+    }
+    return 0;
+    
+}
+
